d3d7076fb4895287ade25b3a4c0d7a047ad3cf1a
Vahid Shaikh <meet2vah1d@gmail.com>
2024-04-22 16:21:19 +0530

    export all utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 library flutter_utils;
 
-export 'constants.dart';
+export 'utils/data_utils.dart';
 export 'utils/file_utils.dart';
 export 'utils/http_utils.dart';
 export 'utils/image_utils.dart';
 export 'utils/json_utils.dart';
 export 'utils/log_utils.dart';
+export 'utils/permission_utils.dart';
+export 'utils/time_utils.dart';
 export 'utils/video_utils.dart';
 export 'utils/widget_utils.dart';
